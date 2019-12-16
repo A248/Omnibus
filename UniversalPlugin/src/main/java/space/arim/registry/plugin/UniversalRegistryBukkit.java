f4f614de42eb02cda1681e502a09c6a5310f4059
A248 <anandebeh@gmail.com>
2019-12-16 09:52:10 -0500

    Add UniversalPlugin and update dependencies

---
+++ UniversalPlugin/src/main/java/space/arim/registry/plugin/UniversalRegistryBukkit.java
@@ -0,0 +1,30 @@
+/* 
+ * UniversalPlugin, a shaded implementation of UniversalRegistry
+ * Copyright © 2019 Anand Beh <https://www.arim.space>
+ * 
+ * UniversalPlugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UniversalPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with UniversalPlugin. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.registry.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UniversalRegistryBukkit extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Loaded all UniversalRegistry classes!");
+	}
+	
+}
