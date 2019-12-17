7afd795a9d2fa91340a47894b0a0b9628a14a3c8
A248 <anandebeh@gmail.com>
2019-12-17 15:50:21 -0500

    Add HttpStatusException and more UniversalUtil methods

---
+++ UniversalUtil/src/main/java/space/arim/registry/util/exception/HttpStatusException.java
@@ -0,0 +1,34 @@
+/* 
+ * ArimBansLib, an API for ArimBans
+ * Copyright © 2019 Anand Beh <https://www.arim.space>
+ * 
+ * ArimBansLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimBansLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimBansLib. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.registry.util.exception;
+
+import space.arim.registry.util.web.HttpStatus;
+
+public class HttpStatusException extends Exception {
+
+	private static final long serialVersionUID = -744115099274403312L;
+	
+	public final HttpStatus status;
+
+	public HttpStatusException(HttpStatus status) {
+		super("Encountered HttpStatus " + status.getCode() + ": " + status.getName());
+		this.status = status;
+	}
+
+}
