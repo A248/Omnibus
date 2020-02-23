338b3f93b87c81686e44e0a356e53ceee6c5c8a3
A248 <anandebeh@gmail.com>
2020-02-23 17:37:58 -0500

    Add @RequireRegistration

---
+++ UniversalRegistry/src/main/java/space/arim/universal/registry/RequireRegistration.java
@@ -0,0 +1,44 @@
+/* 
+ * ArimAPI, a minecraft plugin library and framework.
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * ArimAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimAPI. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.universal.registry;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Denotes a requirement that specific service types be registered in a {@link Registry}. <br>
+ * Therefore, this annotation should be applied to <code>Registry</code> parameters or method specifications (e.g., in interfaces) returning a <code>Registry</code>. <br>
+ * <br>
+ * If applied to a parameter, the parameter supplied should have the service types registered. <br>
+ * If applied to a method in an interface or abstract class, the implementation should return a registry with the service types registered.
+ * 
+ * @author A248
+ *
+ */
+@Retention(SOURCE)
+@Target({ METHOD, PARAMETER })
+public @interface RequireRegistration {
+
+	Class<? extends Registrable>[] value();
+	
+}
