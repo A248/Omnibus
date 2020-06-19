63d8ea033f11eb1f9c17fc1f82301437cd3c831e
A248 <anandebeh@gmail.com>
2020-06-19 12:00:50 -0400

    Add and test ThisClass utility

---
+++ UniversalUtil/src/test/java/space/arim/universal/util/ThisClassTest.java
@@ -0,0 +1,32 @@
+/* 
+ * UniversalUtil
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * UniversalUtil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UniversalUtil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with UniversalUtil. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.universal.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ThisClassTest {
+
+	@Test
+	public void testGet() {
+		assertEquals(ThisClassTest.class, ThisClass.get());
+	}
+	
+}
