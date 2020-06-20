5ca9b91b2e476f03742bc4b6e14d948201d3a49c
A248 <anandebeh@gmail.com>
2020-06-20 17:52:40 -0400

    Overhaul events specified by Registry framework

---
+++ UniversalRegistry/src/main/java/space/arim/universal/registry/RegistryEvent.java
@@ -0,0 +1,45 @@
+/* 
+ * UniversalRegistry
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * UniversalRegistry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UniversalRegistry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with UniversalRegistry. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.universal.registry;
+
+import space.arim.universal.events.Event;
+
+/**
+ * An event related to a registration or unregistration which happened in a {@link Registry}. <br>
+ * <br>
+ * Called to <i>indicate</i> some consequence, but not necessarily called <i>when</i> it happened. <br>
+ * That is, <b>this event may or may not run in the same thread from which the happening itself was occurred.</b> It may run
+ * completely asynchronously regardless. <br>
+ * <br>
+ * It is up to the discretion of {@link Registry} implementations to decide when this event and its subclasses may fire.
+ * Do not assume this event will fire immediately after things literally occur.
+ * 
+ * @author A248
+ *
+ */
+public interface RegistryEvent<T> extends Event {
+
+	/**
+	 * Gets the service involved in this event
+	 * 
+	 * @return the service type
+	 */
+	Class<T> getService();
+	
+}
---
+++ UniversalRegistry/src/main/java/space/arim/universal/registry/ServiceChangeEvent.java
@@ -0,0 +1,51 @@
+/* 
+ * UniversalRegistry
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * UniversalRegistry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * UniversalRegistry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with UniversalRegistry. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.universal.registry;
+
+/**
+ * Indicates the prioritised provider for a specific service changed. This happens
+ * whenever a new registration is added with a higher priority than the existing
+ * highest priority registration, or the highest priority registration was unregistered.
+ * 
+ * @author A248
+ *
+ * @param <T> the service type
+ */
+public interface ServiceChangeEvent<T> extends RegistryEvent<T> {
+
+	/**
+	 * Gets the previous highest priority registration. <br>
+	 * If there was no previous registration when the updated registration
+	 * was registered, this will be {@code null}.
+	 * 
+	 * @return the previous registration, or {@code null} if there was none
+	 */
+	Registration<T> getPrevious();
+	
+	/**
+	 * Gets the updated highest priority registration. <br>
+	 * If the current highest priority registration was unregistered
+	 * and there is no other registration for the same service, this
+	 * will be {@code null}.
+	 * 
+	 * @return the updated registration, or {@code null} if there is none
+	 */
+	Registration<T> getUpdated();
+	
+}
