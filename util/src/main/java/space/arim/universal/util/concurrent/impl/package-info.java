002f5d3b565b718dd7b7a660a51f1e824c6c5128
A248 <anandebeh@gmail.com>
2020-06-23 19:54:13 -0400

    Add package-info for space.arim.universal.util.concurrent and .impl

---
+++ util/src/main/java/space/arim/universal/util/concurrent/impl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains base implementations for interfaces from the concurrency frameworks
+ * defined by {@link space.arim.universal.util.concurrent}
+ * 
+ */
+package space.arim.universal.util.concurrent.impl;
