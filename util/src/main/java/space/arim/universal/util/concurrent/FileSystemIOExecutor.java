3908247f27f3ee7db89df2f8af07b1c96d613127
A248 <anandebeh@gmail.com>
2020-07-12 18:38:22 -0400

    Add Network- and FileSystem- IOExecutor's

---
+++ util/src/main/java/space/arim/universal/util/concurrent/FileSystemIOExecutor.java
@@ -0,0 +1,44 @@
+/* 
+ * Universal-util
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * Universal-util is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Universal-util is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Universal-util. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.universal.util.concurrent;
+
+import java.util.concurrent.Executor;
+
+/**
+ * An executor whose details are tuned for specialising in handling work related to filesystem IO. <br>
+ * <Br>
+ * Such executors typically manage more threads than a Fork/Join pool but less than a
+ * {@link NetworkIOExecutor}. <br>
+ * <br>
+ * This interface is intended as a marker for filesystem IO executors, e.g. as a service in some kind
+ * of registry.
+ * 
+ * @author A248
+ *
+ */
+public interface FileSystemIOExecutor extends Executor {
+
+	/**
+	 * Executes an operation involving considerable filesystem IO
+	 * 
+	 */
+	@Override
+	void execute(Runnable command);
+	
+}
