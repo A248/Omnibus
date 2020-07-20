12abec429ebd4a955c9871a3e821d923566bbb95
A248 <anandebeh@gmail.com>
2020-07-20 19:28:37 -0400

    Update license headers in all files to reflect name revision

--- events/src/test/java/space/arim/omnibus/events/IntOperatorTestListener.java
+++ events/src/test/java/space/arim/omnibus/events/IntOperatorTestListener.java
@@ -1,19 +1,19 @@
 /* 
- * UniversalEvents
+ * Omnibus-events
  * Copyright © 2020 Anand Beh <https://www.arim.space>
  * 
- * UniversalEvents is free software: you can redistribute it and/or modify
+ * Omnibus-events is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * UniversalEvents is distributed in the hope that it will be useful,
+ * Omnibus-events is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with UniversalEvents. If not, see <https://www.gnu.org/licenses/>
+ * along with Omnibus-events. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
 package space.arim.omnibus.events;
--- util/src/main/java/space/arim/omnibus/util/concurrent/CompetitiveFuture.java
+++ util/src/main/java/space/arim/omnibus/util/concurrent/CompetitiveFuture.java
@@ -1,19 +1,19 @@
 /* 
- * UniversalUtil
+ * Omnibus-util
  * Copyright © 2020 Anand Beh <https://www.arim.space>
  * 
- * UniversalUtil is free software: you can redistribute it and/or modify
+ * Omnibus-util is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * UniversalUtil is distributed in the hope that it will be useful,
+ * Omnibus-util is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with UniversalUtil. If not, see <https://www.gnu.org/licenses/>
+ * along with Omnibus-util. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
 package space.arim.omnibus.util.concurrent;
